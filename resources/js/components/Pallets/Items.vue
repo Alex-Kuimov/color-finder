db7c5484660748bf9cb2b600b159c249eec1bfa5
Alex Kuimov <spoot1986@gmail.com>
2023-03-07 21:10:54 +0300

    Bug fix

--- resources/js/components/Pallets/Items.vue
+++ resources/js/components/Pallets/Items.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="mb-4">
-        <p v-if="Object.keys(items).length > 0" class="lead">
-            <Preloader :preloader="preloader"></Preloader>
-            {{ text }}
-        </p>
-        <div class="pallet-wrap">
-            <Preloader :preloader="preloader"></Preloader>
-            <div  v-for="item in items" :key="item.id">
-                <div v-if="Array.isArray(item)" class="pallet-item">
-                    <div class="pallet-color" v-for="color in item" :key="color.id" :style="'background:'+color">
-                        <div class="pallet-text" @click="action.copyColor(color)">{{ color }}</div>
+    <div v-if="Object.keys(items).length > 0">
+        <div class="mb-4">
+            <p class="lead">
+                <Preloader :preloader="preloader"></Preloader>
+                {{ text }}
+            </p>
+            <div class="pallet-wrap">
+                <Preloader :preloader="preloader"></Preloader>
+                <div  v-for="item in items" :key="item.id">
+                    <div v-if="Array.isArray(item)" class="pallet-item">
+                        <div class="pallet-color" v-for="color in item" :key="color.id" :style="'background:'+color">
+                            <div class="pallet-text" @click="action.copyColor(color)">{{ color }}</div>
+                        </div>
                     </div>
+                    <div v-else class="pallet-item">
+                        <div class="pallet-color" :style="'background:'+item">
+                            <div class="pallet-text" @click="action.copyColor(item)">{{ item }}</div>
+                        </div>
+                    </div>      
                 </div>
-                <div v-else class="pallet-item">
-                    <div class="pallet-color" :style="'background:'+item">
-                        <div class="pallet-text" @click="action.copyColor(item)">{{ item }}</div>
-                    </div>
-                </div>      
-            </div>
+            </div>    
         </div>    
     </div>    
 </template>
